6fc4601687dbb35ec975f07e890119933ddbba35
EvgenyMyznikov <evgenymyz@gmail.com>
2021-02-26 02:23:41 +0300

    modified file_server/views.py

--- file_server/app/views.py
+++ file_server/app/views.py
@@ -25,10 +25,8 @@ def file_list(request, date=None):
 			'ctime': file_ctime,
 			'mtime': file_mtime
 		})
-	# if date:
-	# 	srv_list = list(filter(lambda x: x['created_at'] == date, srv_list))
-		if date and not (date == file_ctime.date() or date == file_mtime.date()):
-			continue
+	if date:
+		srv_list = list(filter(lambda x: x['ctime'] == date, srv_list))
 	return render(
 		request,
 		template_name,
